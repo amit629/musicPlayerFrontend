807a7f1c11dd81d257a0e55dc50734af8dbe8060
amit629 <amit0062.be20@chitkara.edu.in>
2024-05-05 20:40:53 +0530

    restart

--- src/app/player/_playerComponent/SideBar.tsx
+++ src/app/player/_playerComponent/SideBar.tsx
@@ -7,7 +7,9 @@ export default function SideBar({bgColor}:{bgColor:string}) {
     const {data}:any=useSession();
   return (
     <div className="h-full w-full rounded-2xl " style={{backgroundColor:bgColor}}>
-          <h1 className="text-[#7D8994] text-5xl text-center font-bold px-6 py-4">MusicAL</h1>
+          <h1 className="text-[#7D8994] text-5xl text-center font-bold px-6 py-4"> 
+          MusicAL 
+          </h1>
       </div>
 
   )
